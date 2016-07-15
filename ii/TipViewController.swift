7e235ff6094c8bd3593ae33d41bb876c0ba222df
Andriy Sergiyenko <andriy.sergiyenko@airbnb.com>
2016-07-14 22:16:52 -0700

    initial commit

---
+++ ii/TipViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ii
+//
+//  Created by andriy_sergiyenko on 7/14/16.
+//  Copyright © 2016 Airbnb. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
